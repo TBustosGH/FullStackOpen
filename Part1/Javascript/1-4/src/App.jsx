8983bf1097f317dd44c7f888723990c3e89194c1
TBustosGH <tnbustos2007@gmail.com>
2025-11-16 12:14:11 -0300

    feat(1-4): Add the fourth exercise of the first part

---
+++ Part1/Javascript/1-4/src/App.jsx
@@ -0,0 +1,52 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
+      <p>{props.parts[1].name} {props.parts[1].exercises}</p>
+      <p>{props.parts[2].name} {props.parts[2].exercises}</p>
+    </div>
+  )
+}
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name :'Fundamentals of react', 
+      exercises : 10
+    },
+    {
+      name : 'Using prop to pass data', 
+      exercises : 7
+    },
+    {
+      name : 'State of a component', 
+      exercises : 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content parts = {parts}/>
+      <Total parts = {parts} />
+    </div>
+  )
+}
+
+export default App
+
