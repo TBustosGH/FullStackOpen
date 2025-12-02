e9acd5e7114273c8620018338f4ee9b1e87cc44d
TBustosGH <tnbustos2007@gmail.com>
2025-12-02 15:27:36 -0300

    feat: Exercise from 3a -Add the exercises from 3-1 to 3-6 (phonebook backend) from 3a

--- Part3/Node.js & Express/example/index.js
+++ Part3/Node.js & Express/example/index.js
@@ -1,22 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
-    }
+    {   id: 1,  content: "HTML is easy",    important: true },
+    {   id: 2,  content: "Browser can execute only JavaScript", important: false    },
+    {   id: 3,  content: "GET and POST are the most important methods of HTTP protocol",    important: true }
 ]
 
 app.get('/', (request, response) => {
@@ -26,10 +16,44 @@ app.get('/', (request, response) => {
 app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
+
+
+const generateID = () => {
+    const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+    console.log('MaxID', maxId)
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        id : generateID(),
+        content : body.content,
+        important : Boolean(body.important) || false
+    }
+
+    console.log('NOTE: ', note)
+
+    notes = notes.concat(note)
+    console.log('Notes: ', notes)
+
+    response.json(note)
+})
+
+
 app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     const note = notes.filter(note => note.id !== id)
-    
+
     response.status(204).end()
 })
 
