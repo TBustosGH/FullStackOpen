7ce688353a613279219137f87331ce2255b04e57
TBustosGH <tnbustos2007@gmail.com>
2025-12-18 12:38:49 -0300

    Refactor: Decide to not be using Express-async-errors -Uninstall express-async-errors dependency (was causing some troubles with express dependency)

--- Part4/Notes (backend)/test/notes_api.test.js
+++ Part4/Notes (backend)/test/notes_api.test.js
@@ -15,10 +15,12 @@ const api = supertest(app)
 
 beforeEach(async () => {
     await Note.deleteMany({})
-    let noteObject = new Note(helper.initialNotes[0])
-    await noteObject.save()
-    noteObject = new Note(helper.initialNotes[1])
-    await noteObject.save()
+
+    const noteObjects = helper.initialNotes
+        .map(note => new Note(note))
+
+    const promiseArray = noteObjects.map(note => note.save())
+    await Promise.all(promiseArray)
 })
 
 describe('Test on Mongo test DB (TestNoteApp)', ()=> {
