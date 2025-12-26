5a5d27763f351d0a3518760562cb1baea9ddb0d7
TBustosGH <tnbustos2007@gmail.com>
2025-12-26 13:42:06 -0300

    Refactor: -Change the note creation api (notesRouter) to use JWT -Add some new JWT-based exceptions on middleware

--- Part4/Notes (backend)/controllers/notes.js
+++ Part4/Notes (backend)/controllers/notes.js
@@ -1,6 +1,20 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
+//METHODS
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
+
+//API ROUTES
 
 notesRouter.get('/', async (request, response) => {
     const notes = await Note
@@ -26,9 +40,13 @@ notesRouter.get('/:id', async (request, response, next) => {
 
 notesRouter.post('/', async (request, response, next) => {
     const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
 
-    const user = await User.findById(body.userId)
-    
+    if (!decodedToken) {
+        return response.status(401).json({ error: 'invalid token' })
+    }
+    const user = await User.findById(decodedToken.id)
+
     try {
         const note = new Note({
             content: body.content,
