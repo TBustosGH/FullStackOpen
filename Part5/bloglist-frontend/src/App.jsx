b4204fb90210e6a6cd9e2b583a10ae63bc371ce3
TBustosGH <tnbustos2007@gmail.com>
2026-01-07 18:15:54 -0300

    Feat: second exercise from 5-1 -Add logout button

--- Part5/bloglist-frontend/src/App.jsx
+++ Part5/bloglist-frontend/src/App.jsx
@@ -70,7 +70,11 @@ const App = () => {
     </form>
   )
   //LOGOUT
-  
+  const handleLogout = async () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    blogService.setToken(null)
+    setUser(null)
+  }
 
 
   if(user === null) {
@@ -85,7 +89,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged-in</p>
+      <p>{user.name} logged-in  <button onClick={handleLogout}>logout</button></p>
       {blogs.map(blog => 
         <Blog key={blog.id} blog={blog}/>
       )}
