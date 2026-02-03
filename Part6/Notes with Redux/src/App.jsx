865079b56f3e9ad195e183595b74d10b95b1d3f1
TBustosGH <tnbustos2007@gmail.com>
2026-02-03 14:13:42 -0300

    Feat: examples from 6-1 -Add modules: vitest, deep freeze -Add reducers folder -Move noteReducer from /src to /src/reducers -Add noteReducer.test.jsx in /src/reducers

--- Part6/Notes with Redux/src/App.jsx
+++ Part6/Notes with Redux/src/App.jsx
@@ -1,6 +1,27 @@
-import store from './store.jsx'
+import noteReducer from './reducers/noteReducer'
+import { createStore } from 'redux'
+
+const store = createStore(noteReducer)
+store.dispatch({ 
+    type: 'NEW_NOTE',
+    payload: {
+        content: 'the app state is in redux store',
+        important: true,
+        id: 1
+    }
+})
+store.dispatch({ 
+    type: 'NEW_NOTE',
+    payload: {
+        content: 'state changes are made with actions',
+        important: false,
+        id: 2
+    }
+})
+
 
 const App = () => {
+  
   console.log('FROM APP', store.getState())
   return(
     <div>
