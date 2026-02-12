b6726ee386ea930436540abd23b93aea3fe568f5
TBustosGH <tnbustos2007@gmail.com>
2026-02-12 09:10:20 -0300

    Feat: example from 6-2 -Add a filter (all, important, non important) to notes with redux -Add a new redux, filterRedux -Combine notesRedux with filterRedux

--- Part6/Notes with Redux/src/main.jsx
+++ Part6/Notes with Redux/src/main.jsx
@@ -1,11 +1,17 @@
 import { createRoot } from 'react-dom/client'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './App.jsx'
-import noteReducer from './reducers/noteReducer.jsx'
+import noteReducer, { createNote } from './reducers/noteReducer.jsx'
+import filterReducer, { filterChange } from './reducers/filterReducer.jsx'
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer)
 
 const root = createRoot(document.getElementById('root'))
 const renderApp = () => {
@@ -15,6 +21,7 @@ const renderApp = () => {
   </Provider>
   )
 }
+
 
 renderApp()
 
