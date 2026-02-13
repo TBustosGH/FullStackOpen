7b418b16a2ab8034aac1e21fdf866b42e0a15f6a
TBustosGH <tnbustos2007@gmail.com>
2026-02-13 09:04:25 -0300

    Refactor: reduxjs/toolkit example from part 6-2 -Install @reduxjs/toolkit into Notes with Redux -Change main.jsx to use configureStore instead of createStore and combineReducer -Move configureStore to its own file, src/store.js; to simplify main.jsx

--- Part6/Notes with Redux/src/main.jsx
+++ Part6/Notes with Redux/src/main.jsx
@@ -1,17 +1,9 @@
 import { createRoot } from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
 import App from './App.jsx'
-import noteReducer, { createNote } from './reducers/noteReducer.jsx'
-import filterReducer, { filterChange } from './reducers/filterReducer.jsx'
-
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
-
-const store = createStore(reducer)
+import store from './store.js'
 
 const root = createRoot(document.getElementById('root'))
 const renderApp = () => {
