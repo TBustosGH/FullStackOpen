b6122f2cdb47c0c511f04961efa0d92440a57192
TBustosGH <tnbustos2007@gmail.com>
2026-03-14 19:25:57 -0300

    Feat: exercise 7-1 from part 7-1 ---Part7/routed-anecdotes--- -Install react-router-dom -Add a BrowserRouter component into main.jsx -Add Link & Route components into App.jsx, to navigate through different views of the page

--- Part7/routed-anecdotes/src/App.jsx
+++ Part7/routed-anecdotes/src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
 
-const Menu = () => {
+import {
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useNavigate,
+  useMatch
+} from 'react-router-dom'
+
+//Components
+const Menu = ({ anecdotes, addNew }) => {
   const padding = {
     paddingRight: 5
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <div>
+        <Link to='/'>anecdotes</Link>
+        <Link to='/create'>create new</Link>
+        <Link to='/about'>about</Link>
+      </div>
+
+      <Routes>
+        <Route path='/' element={<AnecdoteList anecdotes={anecdotes}/>} />
+        <Route path='/create' element={<CreateNew addNew={addNew}/>} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </div>
   )
 }
@@ -83,6 +101,7 @@ const CreateNew = (props) => {
 
 }
 
+//APP component
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
     {
@@ -103,9 +122,13 @@ const App = () => {
 
   const [notification, setNotification] = useState('')
 
+  const navigate = useNavigate()
+  
   const addNew = (anecdote) => {
+
     anecdote.id = Math.round(Math.random() * 10000)
     setAnecdotes(anecdotes.concat(anecdote))
+    navigate('/')
   }
 
   const anecdoteById = (id) =>
@@ -125,10 +148,7 @@ const App = () => {
   return (
     <div>
       <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Menu anecdotes={anecdotes} addNew={addNew}/>
       <Footer />
     </div>
   )
