276c4d53902406d81fb513d0427db80b1f741b46
TBustosGH <tnbustos2007@gmail.com>
2026-03-25 08:46:05 -0300

    Feat: example from part 7-4 ---webpack-part7--- -Add a css file, src/index.css -Install webpack-dev-server -Configure webpack-dev-server, to debug code easily -Install json-server -Configure json-server

--- Part7/webpack-part7/src/App.js
+++ Part7/webpack-part7/src/App.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import './index.css'
+
+
+const useNotes = (url) => {
+    const [notes, setNotes] = useState([])
+    useEffect(() => {
+        axios.get(url).then(response => {
+            setNotes(response.data)
+        })
+    }, [url])
+    return notes
+}
 
 const App = () => {
-    react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(
-    'div',
-    null,
-    'hello webpack'
+    const [counter, setCounter] = useState(0)
+    const [values, setValues] = useState([])
+    const notes = useNotes(BACKEND_URL)
+
+    const handleClick = () => {
+        setCounter(counter + 1)
+        setValues(values.concat(counter))
+    }
+
+    return (
+        <div className='container'>
+            hello webpack {counter} clicks
+            <button onClick={handleClick}>press</button>
+            <div>{notes.length} notes on server, {BACKEND_URL}</div>
+        </div>
     )
 }
 
--- Part7/webpack-part7/webpack.config.js
+++ Part7/webpack-part7/webpack.config.js
@@ -1,12 +1,26 @@
 const path = require('path')
+const webpack = require('webpack')
 
-const config = () => {
+const config = (env, argv) => {
+    console.log('argv', argv.mode)
+
+    const backend_url = argv.mode === 'production'
+        ? 'https://notes2023.fly.dev/api/notes'
+        : 'http://localhost:3001/notes'
     return {
         entry: './src/index.js',
         output: {
             path: path.resolve(__dirname, 'build'),
             filename: 'main.js'
         },
+        devServer: {
+            static: {
+                directory: path.resolve(__dirname, 'build')
+            },
+            compress: true,
+            port: 3000
+        },
+        devtool: 'source-map',
         module: {
             rules: [
                 {
@@ -15,9 +29,18 @@ const config = () => {
                     options: {
                         presets: ['@babel/preset-env', '@babel/preset-react']
                     }
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
                 }
             ]
-        }
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                BACKEND_URL: JSON.stringify(backend_url)
+            })
+        ]
     }
 }
 
