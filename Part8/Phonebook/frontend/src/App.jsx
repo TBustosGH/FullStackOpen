8a21369930314f281d7d969cce544a6f987c87c1
TBustosGH <tnbustos2007@gmail.com>
2026-04-07 09:07:25 -0300

    Feat: example from part 8-1 -Create Part8 folder -Create GraphQL folder into Part8 ---GraphQL--- -Install @apollo/server graphql -Add index.js -Add graphql types (Address, Person, Query) and mutations (addPerson, editNumber) -Add resolvers (Query, Person, Mutation) -Add a simple configuration for the apollo server

---
+++ Part8/Phonebook/frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import PersonFilter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import ShowPersonsInDB from './components/ShowPersonsInDB'
+import {SuccessNotification, ErrorNotification} from './components/Notification'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newPersons, setNewPersons] = useState([])  //To save new persons on a list
+  const [newName, setNewName] = useState('')  //To save the current 'new Name' input
+  const [newNumber, setNewNumber] = useState('')  //To save the current 'new Number'input
+  const [filterName, setFilterName] = useState('')  //To filter names
+  const [successMessage, setSuccessMessage] = useState(null) //To manage a message (success)
+  const [errorMessage, setErrorMessage] = useState(null) //To manage a message (error)
+  
+
+
+  return(
+    <div>
+      <h1>Phonebook</h1>
+      <SuccessNotification message={successMessage} />
+      <ErrorNotification message={errorMessage} />
+      <h2>Search</h2>
+      <PersonFilter filterName = {filterName} setFilterName = {setFilterName}/>
+
+      <h2>Add a new contact</h2>
+      <PersonForm persons={persons} setPersons={setPersons} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber}setSuccessMessage={setSuccessMessage} setErrorMessage={setErrorMessage}/>
+
+      <h2>Numbers</h2>
+      <ol>
+        <ShowPersonsInDB persons={persons} setPersons={setPersons} filterName={filterName}/>
+      </ol>
+    </div>
+  )
+}
+
+export default App
+
