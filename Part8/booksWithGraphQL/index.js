3f8d6e82320e58c4535de57752fc6b6fc02808af
TBustosGH <tnbustos2007@gmail.com>
2026-04-07 22:43:08 -0300

    Feat: exercise 8-2 from part 8-1 ---booksWithGraphQL--- -Add allBooks into Query (typeDefs) -dd allBooks into Query (resolvers)

--- Part8/booksWithGraphQL/index.js
+++ Part8/booksWithGraphQL/index.js
@@ -99,13 +99,15 @@ const typeDefs = `
     type Query {
         authorCount: Int!
         bookCount: Int!
+        allBooks: [Book!]!
     }
 `
 
 const resolvers = {
     Query: {
         authorCount: () => authors.length,
-        bookCount: () => books.length
+        bookCount: () => books.length,
+        allBooks: () => books
     }
 }
 
