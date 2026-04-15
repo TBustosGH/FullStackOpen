87ee48bb931ee7795d9ae4bf57c80c4fe91e7b63
TBustosGH <tnbustos2007@gmail.com>
2026-04-15 09:17:19 -0300

    Feat: exercise 8-8 from part 8-2 -Clone library-frontend ---library-frontend--- -Install @apolloClient and graphql -Add queries/queries.js, with queries ALL_AUTHORS, ALL_BOOKS and ADD_BOOK -Change main.jsx, to import from @apollo/client and @apollo/client/react, create the client and provide it to App -Change components/Authors.jsx, to use ALL_AUTHORS query to fetch authors from backend

---
+++ Part8/library-frontend/src/components/Authors.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import { useQuery } from '@apollo/client/react'
+//QUERIES
+import { ALL_AUTHORS } from '../queries/queries.js'
+
+
+const Authors = (props) => {
+  if (!props.show) {
+    return null
+  }
+
+  const result = useQuery(ALL_AUTHORS)
+  const [authors, setAuthors] = useState([])
+
+  useEffect(() => {
+      if (result.data) {
+          setAuthors(result.data.allAuthors)
+      }
+  }, [result])
+
+
+  if (result.loading) {
+      return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.id}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Authors
---
+++ Part8/library-frontend/src/components/Books.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import { useQuery } from '@apollo/client/react'
+//COMPONENTS
+import { ALL_BOOKS } from '../queries/queries.js'
+
+const Books = (props) => {
+    if (!props.show) {
+        return null
+    }
+
+    const [books, setBooks] = useState([])
+    const result = useQuery(ALL_BOOKS)
+
+    useEffect(() => {
+        if (result.data) {
+            setBooks(result.data.allBooks)
+        }
+    }, [result])
+  
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {books.map((a) => (
+            <tr key={a.id}>
+              <td>{a.title}</td>
+              <td>{a.author}</td>
+              <td>{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Books
