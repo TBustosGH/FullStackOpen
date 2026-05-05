0556ec9da2256ce477bfc9501f1268ebaa292bfd
TBustosGH <tnbustos2007@gmail.com>
2026-05-05 10:44:47 -0300

    Feat: example from part 9-3 ---ilariFlightDiary--- -Add type NewDiaryEntry to types.ts -Change types Weather and Visibility to enums -Add utils.ts -Add functions to validate input data types, isString, isDate, isWeather, isVisibility, to utils.ts -Add functions to parse and validate input data, parseComment, parseDate, parseWeather, parseVisibility, to utils.ts -Add toNewDairyEntry to utils.ts, which uses the functions to parse and validate input data to validate a new diary entry -Change data/entries.ts, to use toNewDiaryEntry to parse and validate all saved data and return a new object with validated data -Change addEntry, from src/services/diaryServices.ts, to addDiary, and make it work, creating and saving a new diary entry -Change diaryRouter`s post endpoint, add error management, validation for the input data (using toNewDiaryEntry), and saving data properly

--- Part9/example/ilariFlightDiary/src/services/diaryService.ts
+++ Part9/example/ilariFlightDiary/src/services/diaryService.ts
@@ -1,6 +1,6 @@
 import diaries from '../../data/entries'
 
-import { NonSensitiveDiaryEntry, DiaryEntry } from '../types';
+import { NonSensitiveDiaryEntry, DiaryEntry, NewDiaryEntry } from '../types';
 
 
 const getEntries = (): DiaryEntry[] => {
@@ -16,13 +16,25 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
     }));
 };
 
-const addEntry = () => {
-    return null;
+const findById = (id: number): DiaryEntry | undefined => {
+    const entry = diaries.find(d => d.id === id);
+    return entry;
+}
+
+const addDiary = ( entry: NewDiaryEntry ): DiaryEntry => {
+    const newDiaryEntry = {
+        id: Math.max(...diaries.map(d => d.id)) + 1,
+        ...entry
+    }
+
+    diaries.push(newDiaryEntry);
+    return newDiaryEntry;
 };
 
 
 export default {
     getEntries,
     getNonSensitiveEntries,
-    addEntry
+    findById,
+    addDiary
 };
