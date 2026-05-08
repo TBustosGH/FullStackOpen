6eb7bca6b22e244c826a757c69f4c3447f117456
TBustosGH <tnbustos2007@gmail.com>
2026-05-08 11:40:05 -0300

    Feat: example from part 9-4 ---my-app-name--- -Add type.ts, to define data types and interfaces -Define interfaces, CoursePartBase, CoursePartBasic, CoursePartGroup and CoursePartBackground; in types.ts -Define and export type CoursePart, in type.ts -Import type CoursePart in App.tsx -Define courseName and courseParts in App.tsx -Add kind atribute to every object inside courseParts object, to define their interfaces (using CoursePart type) -Add a forEach function to print courseParts`s values, using a switch to know of which kind they are -Add assertNever function, to throw an error when an unhandled data type (interface) is detected -Add assertNever function to the switch inside the forEach, as a default case, when part.kind is not part of the typed list of cases

--- Part9/example/my-app-name/src/App.tsx
+++ Part9/example/my-app-name/src/App.tsx
@@ -1,5 +1,67 @@
-const App = (): null => {
-    return null;
+import type { CoursePart } from './types';
+
+//App
+const App = () => {
+    const courseName = 'Half Stack application development';
+    const courseParts: CoursePart[] = [
+        {
+            name: "Fundamentals",
+            exerciseCount: 10,
+            description: "This is an awesome course part",
+            kind: 'basic'   //kind atribute defines the especific type of this object
+        },
+        {
+            name: "Using props to pass data",
+            exerciseCount: 7,
+            groupProjectCount: 3,
+            kind: 'group'
+        },
+        {
+            name: "Basics of type Narrowing",
+            exerciseCount: 7,
+            description: "How to go from unknown to string",
+            kind: 'basic'
+        },
+        {
+            name: "Deeper type usage",
+            exerciseCount: 14,
+            description: "Confusing description",
+            backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+            kind: 'background'
+        },
+    ];
+
+    //detect unhandled data types
+    const asserNever = (value: never): never => {
+        throw new Error(
+            `Unhandled descriminated union member: ${JSON.stringify(value)}`
+        );
+    };
+
+    courseParts.forEach(part => {
+        switch (part.kind) {
+            case 'basic':
+                console.log(part.name, part.description, part.exerciseCount);
+                break;
+            case 'group':
+                console.log(part.name, part.exerciseCount, part.groupProjectCount);
+                break;
+            case 'background':
+                console.log(part.name, part.description, part.backgroundMaterial);
+                break;
+            default:
+                return asserNever(part);    //unhandled data type detected, throw an error
+        }   
+    });
+
+    return (
+        <div>
+            <h1>{courseName}</h1>
+            <div>
+                hello
+            </div>
+        </div>
+    );
 }
 
 export default App
