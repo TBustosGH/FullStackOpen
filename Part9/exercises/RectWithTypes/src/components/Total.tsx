9e5309fb5ada1bb9e3395236599496d2cf71848d
TBustosGH <tnbustos2007@gmail.com>
2026-05-08 11:06:57 -0300

    Feat: exercise 9-14 from part 9-4 -Create RectWithTypes (supossed to be ReactWithTypes) ---RectWithTypes--- -Delete assets folder -Delete stylesheets, App.css and Main.css -Change main.tsx, import ReactDOM, App, and render App component -Change App.tsx, define variables, courseName, courseParts and totalExercises; import components, Header, Content, Total; return Header, Content and Total using courseName, courseParts and totalExercises as props -Add components folder -Add Header.tsx to components folder, define interface HeaderProps, receives the course name as a prop and displays it -Add Content.tsx to components folder, define interface ContentProps, receives the parts of the course as an object prop and displays them -Add Total.tsx to components folder, define interface TotalProps, receives the total of exercises of the course as a prop and displays it

---
+++ Part9/exercises/RectWithTypes/src/components/Total.tsx
@@ -0,0 +1,13 @@
+interface TotalProps {
+    exerciseCount: number
+}
+
+const Total = (props: TotalProps) => {
+    return (
+        <p>
+            Number of exercises: {props.exerciseCount}
+        </p>
+    );
+};
+
+export default Total;
