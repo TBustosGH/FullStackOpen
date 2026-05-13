aa2d411564a2e8b2fa8b354243ed75e999dba3d8
TBustosGH <tnbustos2007@gmail.com>
2026-05-13 14:19:20 -0300

    Refactor: example from part 9-5 ---patientor/backend--- -Change data/patients.ts whole content -Add interface BaseEntryPatient and HealthCheckEntry to types.ts -Add enum HealthCheckRating to types.ts

--- Part9/exercises/patientor/backend/src/types.ts
+++ Part9/exercises/patientor/backend/src/types.ts
@@ -10,6 +10,27 @@ export enum Gender {
     Female = 'female',
     Other = 'other'
 }
+
+interface BaseEntryPatient {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<TypeDiagnoses['code']>;
+};
+
+export enum HealthCheckRating {
+    'Healthy' = 0,
+    'LowRisk' = 1,
+    'HighRisk' = 2,
+    'CriticalRisk' = 3
+}
+
+interface HealtCheckEntry extends BaseEntryPatient {
+    type: 'HealthCheck';
+    healthCheckRating: HealthCheckRating;
+}
+
 // eslint-disable-next-lint @typescript-eslint/no-empty-interface
 export interface Entry {
 
